43906455856705775d8eb02cab859e777f856478
AgnesMuita <wamwithamuita@gmail.com>
2023-08-21 23:12:18 +0300

    set up and run tests using cypress

---
+++ cypress/e2e/integrations/payment_spec.js
@@ -0,0 +1,17 @@
+describe('payment', ()=>{
+  it('user can make payment',()=>{
+    //login
+    cy.visit('/')
+    //check account balance
+    //click on pay button
+    //search for user
+    //add amount and note and click pay
+    //return to transactions
+    //go to personal payments
+    //click on payment
+    //verify that payment was made
+    //check account balance for deduction
+
+  })
+
+})  
