c3374aa3cb1f9595600ef249413b51c99fc1f1db
AgnesMuita <wamwithamuita@gmail.com>
2023-08-21 20:18:05 +0300

    add integration tests

--- src/components/TransactionCreateStepTwo.test.js
+++ src/components/TransactionCreateStepTwo.test.js
@@ -5,16 +5,20 @@ import { act } from 'react-dom/test-utils';
 
 import TransactionCreateStepTwo from "./TransactionCreateStepTwo";
 
+// //for unit testing
+// test('on initial render, the pay button is disabled',async()=>{
 
-test('on initial render, the pay button is disabled',async()=>{
+//     render(<TransactionCreateStepTwo sender={{id:"5"}} receiver={{id:"5"}}/>)
+//     expect(await screen.findByRole('button',{name:/pay/i})).toBeDisabled()
 
-    render(<TransactionCreateStepTwo sender={{id:"5"}} receiver={{id:"5"}}/>)
-    expect(await screen.findByRole('button',{name:/pay/i})).toBeDisabled()
-})
+// })
 
 test('if amount and note is entered, the pay button is enabled',async()=>{
 
     render(<TransactionCreateStepTwo sender={{id:"5"}} receiver={{id:"5"}}/>)
+    //added here for integration test
+    expect(await screen.findByRole('button',{name:/pay/i})).toBeDisabled()
+
     userEvent.type(screen.getByPlaceholderText(/amount/i), "50")
     userEvent.type(screen.getByPlaceholderText(/add a note/i), "dinner")
 
